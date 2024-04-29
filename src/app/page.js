86fa6d3e3df969a05ee1bca1f18ae35f464764a1
Kabol Amine <amine.kabol@gmail.com>
2024-04-29 19:18:06 +0200

    home page fixe pour le moment et aboutpage in progress

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,46 @@
+"use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const Homepage = () => {
   return (
-    <div className="h-full flex flex-col px-4 text-xl lg:flex-row sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      {/* IMAGE CONTAINER */}
-      <div className="h-1/2 relative lg:h-full lg:w-1/2">
-        <Image
-          src="/hero.png"
-          alt="Hero Image"
-          className="object-contain"
-          fill
-        />
-        {/* width={616} height={768} */}
-      </div>
-      {/* TEXT CONTAINER */}
-      <div className="h-1/2 flex flex-col gap-8 items-center justify-center lg:h-full lg:w-1/2">
-        {/* TITLE */}
-        <h1 className="text-4xl font-bold md:text-6xl">Front End Dev</h1>
-        {/* DESC */}
-        <p className="md:text-5xl">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique
-          fugit autem ex error ipsam assumenda.
-        </p>
-        {/* BUTTON */}
-        <div className="flex w-full gap-4">
-          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View my work</button>
-          <button className="p-4 rounded-lg ring-1 ring-black">Contact me</button>
+    <motion.div
+    initial={{ y:"-200vh" }}
+    animate={{ y:"0%" }}
+    transition={{ duration: 1 }}
+    className="h-full">
+      <div className="h-full flex flex-col px-4 text-xl lg:flex-row sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* IMAGE CONTAINER */}
+        <div className="h-1/2 relative lg:h-full lg:w-1/2">
+          <Image
+            src="/hero.png"
+            alt="Hero Image"
+            className="object-contain"
+            fill
+          />
+          {/* width={616} height={768} */}
+        </div>
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 flex flex-col gap-8 items-center justify-center lg:h-full lg:w-1/2">
+          {/* TITLE */}
+          <h1 className="text-4xl font-bold md:text-6xl">Front End Dev</h1>
+          {/* DESC */}
+          <p className="md:text-5xl">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique
+            fugit autem ex error ipsam assumenda.
+          </p>
+          {/* BUTTON */}
+          <div className="flex w-full gap-4">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              View my work
+            </button>
+            <button className="p-4 rounded-lg ring-1 ring-black">
+              Contact me
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
---
+++ src/components/transitionProvider.jsx
@@ -0,0 +1,44 @@
+"use client";
+import { AnimatePresence } from "framer-motion";
+import Navbar from "./navbar";
+import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+const TransitionProvider = ({ children }) => {
+  const pathName = usePathname();
+
+  return (
+    <AnimatePresence mode="wait">
+      <div
+        key={pathName}
+        className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100"
+      >
+        <motion.div
+          animate={{ height: "0vh" }}
+          exit={{ height: "140vh" }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+          className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
+        ></motion.div>
+                <motion.div
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          className="fixed m-auto top-0 bottom-0 left-0 right-0 text-8xl text-white cursor-default w-fit h-fit z-50"
+        >{pathName.substring(1)}</motion.div>
+        <motion.div
+          initial={{ height: "140vh" }}
+          animate={{ height: "0vh", transition: {delay: 0.5} }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+          className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
+        ></motion.div>
+        <div className="h-24">
+          <Navbar />
+        </div>
+        <div className="h-[calc(100%-6rem)]">{children}</div>
+      </div>
+    </AnimatePresence>
+  );
+};
+
+export default TransitionProvider;
